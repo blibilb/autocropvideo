5472d3667d5eb6845bc864b04bc6f9f2abe5a076
bliblib <107342396+blibilb@users.noreply.github.com>
2024-01-13 18:30:26 +0530

    Update autocropper.py
    cropimage() takes max contour area instead of first contour

--- autocropper.py
+++ autocropper.py
@@ -91,8 +91,8 @@ class autocrop:
         img = cv2.imread(filepath)
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         _,thresh = cv2.threshold(gray,self.threshold,255,cv2.THRESH_BINARY)
-        contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        cnt = contours[0]
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnt = max(contours, key=cv2.contourArea)
         x,y,w,h = cv2.boundingRect(cnt)
         crop = img[y:y+h,x:x+w]
         cv2.imwrite(f'cropped{filepath}',crop)
